02a27d51e72d4019ebb74c0d636339f0221d5512
David Stranders <dstranders@hotmail.com>
2017-08-30 10:45:19 +0200

    Added author controller and template

---
+++ src/main/java/nl/davidstranders/spring5webapp/controllers/AuthorController.java
@@ -0,0 +1,27 @@
+package nl.davidstranders.spring5webapp.controllers;
+
+import nl.davidstranders.spring5webapp.repositories.AuthorRepository;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Created by Dell on 30-8-2017.
+ */
+@Controller
+public class AuthorController {
+
+    private AuthorRepository authorRepository;
+
+    public AuthorController(AuthorRepository authorRepository) {
+        this.authorRepository = authorRepository;
+    }
+
+    @RequestMapping("/authors")
+    public String getAuthors(Model model){
+
+        model.addAttribute("authors", authorRepository.findAll());
+
+        return "authors";
+    }
+}
